447c728aeb2acee010d691e80af0b9dc27febde9
tnugiel <tnugiel@hotmail.com>
2016-02-04 15:04:51 -0500

    non-working - updated with ADAL plugin code

---
+++ www/services/office365/settings/settings.js
@@ -0,0 +1,8 @@
+
+var O365 = {
+        clientId: '4b1b18fc-c613-423c-a044-4af969938d49',
+        authUri: 'https://login.microsoftonline.com/common/',
+        redirectUri: 'http://localhost:4400/services/office365/redirectTarget.html',
+        domain: 'teammjc.com',
+        tenantId: 'dbfb55e0-cbea-40ad-b2c7-816825d35d76'
+};
